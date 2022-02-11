366f92cb3a7a9dd301366f40b958ff3cfbfca8f8
Anna Kosieradzka <a.wrochna@gmail.com>
2022-02-11 15:15:37 +0100

    Code cleanup

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,6 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
 import { App } from './App';
 import { mergeStyles } from '@fluentui/react';
-import reportWebVitals from './reportWebVitals';
 
 // Inject some global styles
 mergeStyles({
@@ -13,9 +11,6 @@ mergeStyles({
   },
 });
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.body);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
