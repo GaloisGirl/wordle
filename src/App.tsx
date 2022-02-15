c22047e4c84686d181cc823757fb79e1e1db3361
Anna Kosieradzka <a.wrochna@gmail.com>
2022-02-15 13:28:11 +0100

    Added Grid stub

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,11 @@ import { Stack } from '@fluentui/react';
 
 import * as Modals from './modals';
 import { Keyboard } from './Keyboard';
+import { Grid } from './Grid';
 
 export const App: React.FunctionComponent = () => {
   return (
-    <Stack style={{ width: 600, margin: 'auto' }}>
+    <Stack style={{ width: 600, margin: 'auto' }} tokens={{childrenGap: 20}}>
       <Stack horizontal horizontalAlign="center" verticalAlign='center'>
         <Modals.HelpModal></Modals.HelpModal>
         <Modals.WardleModal></Modals.WardleModal>
@@ -14,6 +15,7 @@ export const App: React.FunctionComponent = () => {
         <Modals.StatsModal></Modals.StatsModal>
         <Modals.SettingsModal></Modals.SettingsModal>
       </Stack>
+      <Grid></Grid>
       <Keyboard></Keyboard>
     </Stack>
   );
