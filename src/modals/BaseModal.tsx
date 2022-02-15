0f8f1129907b45e3d0fc4acf9f2ba1262d1b4fe0
Anna Kosieradzka <a.wrochna@gmail.com>
2022-02-15 13:54:20 +0100

    Added BaseModal

---
+++ src/modals/BaseModal.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { useId, useBoolean } from '@fluentui/react-hooks';
+import { Stack, IconButton, Modal } from '@fluentui/react';
+
+interface BaseModalProps {
+    children: React.ReactNode
+    title: string
+    iconName: string
+  }
+
+export const BaseModal: React.FunctionComponent<BaseModalProps> = (props: BaseModalProps) => {
+    const [isModalOpen, { setTrue: showModal, setFalse: hideModal }] = useBoolean(false);
+    const titleId = useId('title');
+
+    return (<div>
+        <IconButton 
+            iconProps={{ iconName: props.iconName }} 
+            onClick={showModal} 
+            aria-label={props.title}
+            title={props.title}
+        >            
+        </IconButton>
+        <Modal
+            titleAriaId={titleId}
+            isOpen={isModalOpen}
+            onDismiss={hideModal}
+            className="wd-modal"
+        >
+            <Stack>
+                <Stack horizontal horizontalAlign="space-between" >
+                    <h2 id={titleId}>{props.title}</h2>
+                    <IconButton
+                        iconProps={{ iconName: 'Cancel' }}
+                        ariaLabel="Close popup modal"
+                        onClick={hideModal}
+                    />
+                </Stack>
+                <div >
+                {props.children}
+                </div>
+            </Stack>
+        </Modal>
+    
+    </div>)
+}
--- src/modals/StatsModal.tsx
+++ src/modals/StatsModal.tsx
@@ -1,41 +1,8 @@
-
 import React from 'react';
-import { useId, useBoolean } from '@fluentui/react-hooks';
-import { Stack, IconButton, Modal } from '@fluentui/react';
-
+import { BaseModal } from './BaseModal';
 
 export const StatsModal: React.FunctionComponent = () => {
-    const [isModalOpen, { setTrue: showModal, setFalse: hideModal }] = useBoolean(false);
-    const titleId = useId('title');
-
-    return (<div>
-        <IconButton
-            iconProps={{ iconName: 'BarChartVertical' }}
-            onClick={showModal}
-            aria-label="Stats"
-            title="Stats"
-        >
-        </IconButton>
-        <Modal
-            titleAriaId={titleId}
-            isOpen={isModalOpen}
-            onDismiss={hideModal}
-            className="wd-modal"
-        >
-            <Stack>
-                <Stack horizontal horizontalAlign="space-between" >
-                    <h2 id={titleId}>Stats</h2>
-                    <IconButton
-                        iconProps={{ iconName: 'Cancel' }}
-                        ariaLabel="Close popup modal"
-                        onClick={hideModal}
-                    />
-                </Stack>
-                <div >
-                    <p>Stats will appear here</p>
-                </div>
-            </Stack>
-        </Modal>
-    </div>);
-
+    return (<BaseModal title="Stats" iconName="BarChartVertical">
+        <p>Stats will appear here</p>
+    </BaseModal>)
 }
